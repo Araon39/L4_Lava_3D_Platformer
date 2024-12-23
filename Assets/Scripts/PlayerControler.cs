45af9f89ca04f71d1f3954464659511d9e790ea2
Araon39 <115640752+Araon39@users.noreply.github.com>
2024-12-23 14:52:13 +0200

    AddControl_Jump
    Добавили контроль прыжков
    Добавили канвас

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,30 +1,44 @@
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private float horizontal;
     private float vertical;
 
-    private int speed = 20; // Скорость перемещения игрока
-    private int force = 10; // Сила импульса
+    private int speed = 20;
+    private int rotationSpeed = 60;
+    public int force = 7;
 
-    public Rigidbody rb; // Ссылка на компонент Rigidbody
+    public bool onIsland;
+
+    public Rigidbody rb;
 
     void Update()
     {
-        // Получаем значения осей ввода для горизонтального и вертикального перемещения
+        // Получаем значения осей ввода
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
-        // Перемещаем объект вперед и вращаем его в зависимости от ввода
+        // Перемещаем объект вперед и вращаем его
         transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical);
-        transform.Rotate(Vector3.up * Time.deltaTime * speed * horizontal);
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * horizontal);
 
-        // Проверяем, нажата ли клавиша пробела
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Проверяем, нажата ли клавиша пробел и находимся ли на острове
+        if (Input.GetKeyDown(KeyCode.Space) && onIsland == true)
         {
-            // Применяем импульс к объекту вверх
+            // Применяем силу к объекту, чтобы он подпрыгнул
             rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+            onIsland = false;
+        }
+    }
+
+    // Метод вызывается при столкновении с другим объектом
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Проверяем, столкнулись ли мы с объектом с тегом "Island"
+        if (collision.gameObject.CompareTag("Island"))
+        {
+            onIsland = true;
         }
     }
 }
